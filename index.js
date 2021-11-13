dcda77cc32d8cc695624ae4c7410ace02475df16
XnoobSpeakable <62246474+XnoobSpeakable@users.noreply.github.com>
2021-11-13 18:14:36 +0200

    dfgvfvg

---
+++ index.js
@@ -0,0 +1,11 @@
+let x = 0
+let y = 1
+function upfunction()
+{
+setInterval(() => {
+    x++
+    console.log(x)
+}, 1000);
+}
+document.getElementById("ID").innerHTML = "<p>Text</p>";
+element.textContent = x
