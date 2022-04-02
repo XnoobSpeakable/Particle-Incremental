6ff7ed404528324786de727a05828eae30d554c7
XnoobSpeakable <62246474+XnoobSpeakable@users.noreply.github.com>
2022-04-02 17:30:43 +0300

    particle incremental rewrritten

--- main.js
+++ main.js
@@ -1,38 +1,92 @@
-let num = 0
-let inc = 1
-let speed = 1000
-let speedup = 2
-
-let uprunning = 0
-
-function upfunction() {
-    setInterval(() => {
-        num += inc
-        document.getElementById("counter").textContent = num + " particles"
-    }, speed);
-}
-
-
-function callup() {
-    if(uprunning == 0) {
-        uprunning = 1
-        upfunction()  
+function load() {
+    if(localStorage.getItem('savefile') == "") {
+        sf = {
+            num: 0,
+            inc: 1,
+            mbinc: 1,
+            firstgenbought: false,
+            gencost: 1000,
+            genmult: 1,
+            mbmultv: 1,
+            bbcost: 2000,
+            speedcost: 50,
+            intervalspeed: 1000,
+            fracmult: 2
+          };
+        }
+    else {
+        sf = JSON.parse(localStorage.getItem('savefile'))
     }
 }
 
+load()
+
+document.getElementById("counter").textContent = sf.num + " particles"
+
+function run() {
+    sf.num += sf.inc * sf.genmult
+    document.getElementById("counter").textContent = sf.num + " particles"
+}
+
+function buygen() {
+        if(sf.firstgenbought == false) {
+            sf.firstgenbought = true
+            var interval = setInterval(run, sf.intervalspeed);
+            document.getElementById("divgencost").textContent = "Cost: " + sf.gencost
+        }
+        if(sf.firstgenbought == true) {
+            if(sf.num >= sf.gencost) {
+                sf.num -= sf.gencost
+                sf.gencost *= 4
+                clearInterval(interval)
+                sf.genmult++
+                document.getElementById("divgencost").textContent = "Cost: " + sf.gencost
+                interval = setInterval(run, sf.intervalspeed);
+            }
+        }
+    }
 
 function buybb() {
-    if(num >= 2000) {
-        num -= 2000
-        inc++
+    if(sf.num >= sf.bbcost) {
+        sf.num -= sf.bbcost
+        sf.bbcost *= 2
+        document.getElementById("divbbcost").textContent = "Cost: " + sf.bbcost
+        clearInterval(interval)
+        sf.inc++
+        interval = setInterval(run, sf.intervalspeed);
     }
 }
 
-function buyspeed() {
-    if(num >= 50) {
-        num -= 50
-        speed = 1000 / speedup
-        speedup++
-    }
 
+function buyspeed() {
+    if(sf.num >= sf.speedcost) {
+        sf.num -= sf.speedcost
+        clearInterval(interval)
+        sf.intervalspeed = 1000 / sf.fracmult
+        sf.interval = setInterval(run, sf.intervalspeed);
+        sf.fracmult++
+    }
+}
+
+function cheat() {
+    sf.num += 1000
+    document.getElementById("counter").textContent = sf.num + " particles"
+}
+/*
+function mbman() {
+    sf.num += sf.mbinc * sf.mbmultv
+    document.getElementById("counter").textContent = sf.num + " particles"
+}
+
+function mbup() {
+
+}
+
+function mbmult() {
+
+}*/
+
+function save() {
+    savefile = JSON.stringify(sf)
+    localStorage.setItem('savefile', savefile)
 }
